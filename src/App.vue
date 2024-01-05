5e08b952b3a5fde48a0023ca93bceba5be7afdc5
Alex Cuvelier <cuvelieralex@gmail.com>
2024-01-05 13:52:40 +0100

    code quality

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="m-2">
         <div class="flex flex-col">
             <label for="sourceDomain" class="mb-1">Source</label>
-            <Dropdown v-model="sourceDomain" inputId="sourceDomain" :options="openedTabsUrls" placeholder="Source" />
+            <Dropdown v-model="sourceDomain" inputId="sourceDomain" :options="openedTabsDomains" placeholder="Source" />
         </div>
 
         <div class="flex flex-col mt-2 max-h-64 overflow-y-auto">
@@ -14,7 +14,7 @@
 
         <div class="flex flex-col mt-2">
             <label for="targetDomain" class="mb-1">Target</label>
-            <Dropdown v-model="targetDomain" inputId="targetDomain" :options="openedTabsUrls" placeholder="Target" />
+            <Dropdown v-model="targetDomain" inputId="targetDomain" :options="openedTabsDomains" placeholder="Target" />
         </div>
 
         <div class="mt-2">
@@ -36,18 +36,20 @@ import Button from 'primevue/button';
 
 const sourceDomain = ref('');
 const targetDomain = ref('');
-const openedTabsUrls = ref([]);
+const openedTabsDomains = ref([]);
+
+//get opened tabs domains on mount and save them in openedTabsDomains
 const getOpenedTabs = () => {
     chrome.tabs.query({}, function (tabs) {
-        openedTabsUrls.value = tabs.map((tab) => new URL(tab.url).hostname);
+        openedTabsDomains.value = tabs.map((tab) => new URL(tab.url).hostname);
     });
 };
 onMounted(getOpenedTabs);
 
 const selectedCookies = ref([]);
-
 const sourceDomainCookies = ref([]);
 
+//can copy if source and target domains are selected and at least one cookie is selected
 const canCopy = computed(() => {
     return sourceDomain.value && targetDomain.value && selectedCookies.value.length;
 });
@@ -56,36 +58,43 @@ const copyDone = ref(false);
 const copyError = ref(false);
 const copyCookies = () => {
     chrome.cookies.getAll({ domain: sourceDomain.value }, function (cookies) {
+
+        //filter cookies to copy
         const cookiesToCopy = cookies.filter((cookie) => selectedCookies.value.includes(cookie.name));
         const promises = [];
+
+        //for each selected cookie, set it on the target domain and save the promise
         cookiesToCopy.forEach((cookie) => {
-            const newCookie = {
+            promises.push(chrome.cookies.set({
                 url: `http://${targetDomain.value}`,
                 name: cookie.name,
                 value: cookie.value,
                 domain: targetDomain.value,
                 path: cookie.path,
                 expirationDate: cookie.expirationDate
-            };
-            console.log('newCookie', newCookie);
-            promises.push(chrome.cookies.set(newCookie));
+            }));
         });
+
+        //when all promises are done, set copyDone or copyError
         Promise.all(promises)
-          .then(() => {
-            copyDone.value = true;
-        }).catch((err) => {
-            copyError.value = true;
-        })
+            .then(() => {
+                copyDone.value = true;
+            })
+            .catch((err) => {
+                console.error(err);
+                copyError.value = true;
+            });
     });
 };
 
-
+//When source domain changes, get cookies for that domain
 watch(sourceDomain, (sourceDomain) => {
     chrome.cookies.getAll({ domain: sourceDomain }, function (cookies) {
         sourceDomainCookies.value = cookies;
     });
 });
 
+//When form changes, reset copy status
 watch([sourceDomain, targetDomain, selectedCookies], () => {
     copyDone.value = false;
     copyError.value = false;
